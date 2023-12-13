0f090e8b53dcdbe5f73c01d6be75514f51101ef7
Karuszz <modkarcsika@gmail.com>
2023-12-13 14:02:13 +0100

    account

--- webshop/assets/js/main.js
+++ webshop/assets/js/main.js
@@ -1,7 +1,24 @@
+//Nav Start
 function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-  }
+  document.getElementById("mySidenav").style.width = "250px";
+}
   
-  function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-  }
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+
+//Nav End
+
+//Account Start
+const container = document.getElementById('container');
+const regBtn = document.getElementById('regist');
+const loginBtn = document.getElementById('login');
+
+regBtn.addEventListener('click', ()=>{
+  container.classList.add('active');
+});
+
+loginBtn.addEventListener('click', ()=>{
+  container.classList.remove('active');
+});
+//Account End
