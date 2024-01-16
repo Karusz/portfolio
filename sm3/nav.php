be870e2fb6028b4f47963667195e5ab31d738698
Karusz <112902245+Karusz@users.noreply.github.com>
2024-01-16 17:27:45 +0100

    bugfix

--- sm3/nav.php
+++ sm3/nav.php
@@ -3,15 +3,15 @@ session_start();
 if(empty($_SESSION['id'])){
     echo '<ul>
     <li class="brand bal"><a>Ticket</a></li>
-    <li class="log-btn jobb"><button><a href="index.php">Kezdolap</a></button></li>
-    <li class="log-btn jobb"><button><a href="regist.php">Regisztráció</a> </button></li>
-    <li class="log-btn jobb"><button><a href="login.php">Bejelentkezés</a> </button></li>
+    <li class="log-btn jobb">a href="index.php"><button><Kezdolap</button></a></li>
+    <li class="log-btn jobb"><a href="regist.php"><button>Regisztráció </button></a></li>
+    <li class="log-btn jobb"><a href="login.php"><button>Bejelentkezés</button></a></li>
 </ul>';
 }else{
     echo '<ul>
     <li class="brand bal"><a>Ticket</a></li>
-    <li class="log-btn jobb"><button><a href="addevent.php">Event hozzáadás</a> </button></li>
-    <li class="log-btn jobb"><button><a href="index.php">Kezdolap</a></button></li>
+    <li class="log-btn jobb"><a href="addevent.php"><button>Event hozzáadás</button></a></li>
+    <li class="log-btn jobb"><a href="index.php"><button>Kezdolap</button></a></li>
 </ul>';
 }
     
