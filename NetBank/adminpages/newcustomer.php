006170906b8f83b90f6179d61c4a1dc85995aba2
Karusz <112902245+Karusz@users.noreply.github.com>
2024-03-06 19:15:35 +0100

    tobbi

--- NetBank/adminpages/newcustomer.php
+++ NetBank/adminpages/newcustomer.php
@@ -1,4 +1,39 @@
 <?php
+    require "../config.php";
+    require "functions.php";
+
+    if(isset($_POST['felv-btn'])){
+        $kname = $_POST['kname'];
+        $vname = $_POST['vname'];
+        $password = $_POST['psw'];
+
+        $azonosito = CodeGenerate(8);
+        //Megnézzük, hogy van-e már olyan kóddal ellátott ügyfél
+        while(mysqli_num_rows($codes = $conn->query("SELECT * FROM users WHERE code='$azonosito'")) == 1){
+            $azonosito = CodeGenerate(8);
+        }
+
+        $szamlaszam = SzamlaGenerate();
+        while (mysqli_num_rows($szamla = $conn->query("SELECT * FROM szamlak WHERE szamlaszam = '$szamlaszam'")) == 1) {
+            $szamlaszam = SzamlaGenerate();
+        }
+        
+        $conn->query("INSERT INTO users VALUES('$azonosito', '$vname','$kname','$password')");
+        $conn->query("INSERT INTO szamlak VALUES('$azonosito','$szamlaszam',0)");
+    }
+
+    if(isset($_POST['up-btn'])){
+        $fel_ember_szamla = $_POST['ember-szamlaszam'];
+        $ertek = $_POST['ertek'];
+
+        $leker = "SELECT * FROM szamlak WHERE szamlaszam = '$fel_ember_szamla'";
+        $talalt = $conn->query($leker);
+        $ab_szamlasz = $talalt->fetch_assoc();
+
+        $ertek += $ab_szamlasz['osszeg'];
+        
+        $conn->query("UPDATE szamlak SET osszeg = $ertek WHERE szamlaszam = '$fel_ember_szamla'");
+    }
 
 ?>
 
@@ -17,12 +52,12 @@
     <header>
         <ul class="navbar">
             <li><a href="newcustomer.php">Új ügyfél</a></li>
-            <li><a href="findcustomer.php">Ügyfél kereső</a></li>
+            <li><a href="index.php">Ügyfél kereső</a></li>
         </ul>
     </header>
     <div class="add-customer">
         <h2>Ügyfél hozzáadása</h2>
-        <form action="admin.php" method="post">
+        <form action="newcustomer.php" method="post">
             <input type="text" name="vname" placeholder="Vezetéknév">
             <input type="text" name="kname" placeholder="Keresztnév">
             <input type="password" name="psw" placeholder="Jelszó">
@@ -32,7 +67,7 @@
     <div class="vonal"></div>
     <div class="add-money">
         <h2>Pénz feltöltése</h2>
-        <form action="admin.php" method="post">
+        <form action="newcustomer.php" method="post">
             <select name="ember-szamlaszam">
                     <option value="">Szamlaszam</option>
             </select>
