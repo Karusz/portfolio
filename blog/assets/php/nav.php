4f3fd378f9fde3b971b729ba242c8568d2438dd1
Karusz <112902245+Karusz@users.noreply.github.com>
2024-06-23 23:15:22 +0200

    create files

---
+++ blog/assets/php/nav.php
@@ -0,0 +1,25 @@
+<?php
+    echo '<nav class="navbar navbar-expand-lg navbar-dark bg-dark" aria-label="Tenth navbar example">
+                <div class="container-fluid">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#nav" aria-controls="nav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse justify-content-md-center" id="nav">
+                    <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" aria-current="page" href="index.html">Kezdolap</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Posztok</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="kapcsolat.html">Kapcsolat</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="login.html">Bejelentkezes</a>
+                    </li>
+                    </ul>
+                </div>
+                </div>
+            </nav>';
+?>
